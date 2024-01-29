1f13059ae8560d9482b6a8e8b5abd779f4453170
Pablo Hernandez <55292284+PabloHdzVizcarra@users.noreply.github.com>
2024-01-28 18:51:10 -0600

    feat added id generator util method

--- src/main/java/com/github/pablohdzvizcarra/DatabaseUtils.java
+++ src/main/java/com/github/pablohdzvizcarra/DatabaseUtils.java
@@ -1,9 +1,15 @@
 package com.github.pablohdzvizcarra;
 
+import java.util.UUID;
+
 public class DatabaseUtils {
     public String getDatabaseNameFormatted(String databaseName) {
         if (databaseName == null)
             throw new NullPointerException("Database name is null");
         return databaseName + ".txt";
+    }
+
+    public String createDocumentId() {
+        return UUID.randomUUID().toString().replace("-", "_");
     }
 }
--- src/test/java/com/github/pablohdzvizcarra/DatabaseUtilsTest.java
+++ src/test/java/com/github/pablohdzvizcarra/DatabaseUtilsTest.java
@@ -1,5 +1,6 @@
 package com.github.pablohdzvizcarra;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -29,5 +30,14 @@ class DatabaseUtilsTest {
         } catch (NullPointerException e) {
             assertEquals("Database name is null", e.getMessage());
         }
+    }
+
+    @Test
+    void shouldCreateRandomId() {
+        String id = databaseUtils.createDocumentId();
+        assertThat(id)
+                .isNotEmpty()
+                .hasSize(36)
+                .doesNotContain("-");
     }
 }
