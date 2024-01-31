226f756a18025790839293b77c96d1c6596ac315
Pablo Hernandez <55292284+PabloHdzVizcarra@users.noreply.github.com>
2024-01-30 21:41:01 -0600

    refactor parse User to POJO

--- src/main/java/com/github/pablohdzvizcarra/User.java
+++ src/main/java/com/github/pablohdzvizcarra/User.java
@@ -1,9 +1,63 @@
 package com.github.pablohdzvizcarra;
 
-public record User(
-        String name,
-        String lastName,
-        String email,
-        String nickname) {
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public class User {
+        private String name;
+        private String lastName;
+        private String email;
+        private String nickname;
+        @JsonAlias("_id")
+        private String id;
+
+        public User() {
+        }
+
+        public User(String name, String lastName, String email, String nickname) {
+                this.name = name;
+                this.lastName = lastName;
+                this.email = email;
+                this.nickname = nickname;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public String getLastName() {
+                return lastName;
+        }
+
+        public void setLastName(String lastName) {
+                this.lastName = lastName;
+        }
+
+        public String getEmail() {
+                return email;
+        }
+
+        public void setEmail(String email) {
+                this.email = email;
+        }
+
+        public String getNickname() {
+                return nickname;
+        }
+
+        public void setNickname(String nickname) {
+                this.nickname = nickname;
+        }
+
+        public String getId() {
+                return id;
+        }
+
+        public void setId(String id) {
+                this.id = id;
+        }
 
 }
