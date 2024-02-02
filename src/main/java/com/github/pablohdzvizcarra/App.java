aeb787124bc63cfdf0224e45f05ff455e2f57d00
Pablo Hernandez <55292284+PabloHdzVizcarra@users.noreply.github.com>
2024-02-01 21:45:29 -0600

    feat added javalin library
    Added Javalin library to create a HTTP server to add a HTTP layer to interact with the database for external users

--- src/main/java/com/github/pablohdzvizcarra/App.java
+++ src/main/java/com/github/pablohdzvizcarra/App.java
@@ -1,7 +1,10 @@
 package com.github.pablohdzvizcarra;
 
+import io.javalin.Javalin;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Javalin app = Javalin.create().start(9182);
+        app.get("/", ctx -> ctx.result("Hello World"));
     }
 }
