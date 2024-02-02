3ef2e60cca396864a71f36d8784e6fdeda1aece1
Pablo Hernandez <55292284+PabloHdzVizcarra@users.noreply.github.com>
2024-02-02 07:32:46 -0600

    feat added HTTP endpoint to delete users

--- src/main/java/com/github/pablohdzvizcarra/App.java
+++ src/main/java/com/github/pablohdzvizcarra/App.java
@@ -6,7 +6,7 @@ public class App {
     private static final Database<User> database = new Database<>("users");
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(9182);
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/", ctx -> ctx.result("Welcome to JSON database!"));
         app.post("/api/v1/{collection}", ctx -> {
             String collection = ctx.pathParam("collection");
             String body = ctx.body();
@@ -24,11 +24,25 @@ public class App {
             String id = ctx.pathParam("id");
             try {
                 String document = database.readDocument(collection, id);
+                ctx.contentType("application/json");
                 ctx.result(document);
             } catch (Exception e) {
                 ctx.status(404);
                 ctx.result("The record cannot be retrieved message error: " + e.getMessage());
             }
         });
+
+        app.delete("api/v1/{collection}/{id}", ctx -> {
+            String collection = ctx.pathParam("collection");
+            String id = ctx.pathParam("id");
+            try {
+                database.deleteDocument(collection, id);
+                ctx.result("The record was deleted");
+            } catch (Exception e) {
+                ctx.status(400);
+                ctx.result("The record cannot be deleted message error: " + e.getMessage());
+            }
+        });
     }
+
 }
